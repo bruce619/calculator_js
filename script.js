f9d0d652120a4151f07d1feb192c26d146d9b340
Ibecheozor Chimuanya Bruce <chimuanyaibechozor@gmail.com>
2021-03-31 22:53:49 +0100

    improved style.css, script.js and index.html

--- script.js
+++ script.js
@@ -1,88 +1,92 @@
 document.addEventListener("DOMContentLoaded", ()=>{
 
-    class Calculator {
+    class myCalculator {
         constructor(previousOperandTextElement, currentOperandTextElement) {
           this.previousOperandTextElement = previousOperandTextElement
           this.currentOperandTextElement = currentOperandTextElement
           this.clear()
         };
+
+        appendNumber(number) {
+            if (number === '.' && this.currentOperand.includes('.')) return
+            this.currentOperand = this.currentOperand.toString() + number.toString()
+        };
+        
+        chooseOperation(operation) {
+            if (this.currentOperand === '') return
+            if (this.previousOperand !== '') {
+                this.compute()
+            };
+            this.operation = operation
+            this.previousOperand = this.currentOperand
+            this.currentOperand = ''
+        };
+        
+        compute() {
+            let computation
+            const previous = parseFloat(this.previousOperand)
+            const current = parseFloat(this.currentOperand)
+            if (isNaN(previous) || isNaN(current)) return
+            switch (this.operation) {
+                case '+':
+                    computation = previous + current
+                    break
+                case '-':
+                    computation = previous - current
+                    break
+                case '*':
+                    computation = previous * current
+                    break
+                case '÷':
+                    computation = previous / current
+                    break
+                default:
+                return
+            };
+
+            this.currentOperand = computation
+            this.operation = undefined
+            this.previousOperand = ''
+            
+        };
+        
+        getDisplayNumber(number) {
+            const stringNumber = number.toString()
+            const integerNumber = parseFloat(stringNumber.split('.')[0])
+            const decimalNumber = stringNumber.split('.')[1]
+            var integerDisplay
+            if (isNaN(integerNumber)) {
+                integerDisplay = ''
+            } else {
+                integerDisplay = integerNumber.toLocaleString('en', { maximumFractionDigits: 0 })
+            };
+            if (decimalNumber != null) {
+                return `${integerDisplay}.${decimalNumber}`
+            }else {
+                return integerDisplay
+            };
+        };
+        
+        updateDisplay() {
+            this.currentOperandTextElement.innerText = this.getDisplayNumber(this.currentOperand)
+            if (this.operation != null) {
+                this.previousOperandTextElement.innerText = `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
+            }else {
+                this.previousOperandTextElement.innerText = ''
+            };
+        };
       
         clear() {
-          this.currentOperand = ''
-          this.previousOperand = ''
-          this.operation = undefined
+            this.operation = undefined
+            this.currentOperand = ''
+            this.previousOperand = ''
+          
         };
       
         delete() {
-          this.currentOperand = this.currentOperand.toString().slice(0, -1)
+            this.currentOperand = this.currentOperand.toString().slice(0, -1)
         };
       
-        appendNumber(number) {
-          if (number === '.' && this.currentOperand.includes('.')) return
-          this.currentOperand = this.currentOperand.toString() + number.toString()
-        };
-      
-        chooseOperation(operation) {
-          if (this.currentOperand === '') return
-          if (this.previousOperand !== '') {
-            this.compute()
-          };
-          this.operation = operation
-          this.previousOperand = this.currentOperand
-          this.currentOperand = ''
-        };
-      
-        compute() {
-          let computation
-          const previous = parseFloat(this.previousOperand)
-          const current = parseFloat(this.currentOperand)
-          if (isNaN(previous) || isNaN(current)) return
-          switch (this.operation) {
-            case '+':
-              computation = previous + current
-              break
-            case '-':
-              computation = previous - current
-              break
-            case '*':
-              computation = previous * current
-              break
-            case '÷':
-              computation = previous / current
-              break
-            default:
-              return
-          };
-          this.currentOperand = computation
-          this.operation = undefined
-          this.previousOperand = ''
-        };
-      
-        getDisplayNumber(number) {
-          const stringNumber = number.toString()
-          const integerNumber = parseFloat(stringNumber.split('.')[0])
-          const decimalNumber = stringNumber.split('.')[1]
-          var integerDisplay
-          if (isNaN(integerNumber)) {
-            integerDisplay = ''
-          } else {
-            integerDisplay = integerNumber.toLocaleString('en', { maximumFractionDigits: 0 })
-          };
-          if (decimalNumber != null) {
-            return `${integerDisplay}.${decimalNumber}`
-          } else {
-            return integerDisplay
-          };
-        };
-      
-        updateDisplay() {
-          this.currentOperandTextElement.innerText = this.getDisplayNumber(this.currentOperand)
-          if (this.operation != null) {
-            this.previousOperandTextElement.innerText = `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
-          } else {
-            this.previousOperandTextElement.innerText = ''
-          };
-        };
       };
       
       
@@ -94,7 +98,7 @@ document.addEventListener("DOMContentLoaded", ()=>{
       const previousOperandTextElement = document.querySelector('[data-previous-operand]')
       const currentOperandTextElement = document.querySelector('[data-current-operand]')
       
-      const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
+      const calculator = new myCalculator(previousOperandTextElement, currentOperandTextElement)
       
       numberButtons.forEach(button => {
         button.addEventListener('click', () => {
